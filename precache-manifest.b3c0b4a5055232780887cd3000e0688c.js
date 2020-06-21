8af01031841c0e9ecacc3bf63acfa991eac14a1a
Guilherme Moura <gmoura.nas@gmail.com>
2020-06-21 00:21:59 -0300

    Updates

---
+++ precache-manifest.b3c0b4a5055232780887cd3000e0688c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316f0d34aec6bc31860212d25cc38125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2d3ed9eb9d689f5c41",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "060ed98104816b4d7490",
+    "url": "/static/js/2.9a34cc03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a34cc03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2d3ed9eb9d689f5c41",
+    "url": "/static/js/main.cded9e7b.chunk.js"
+  },
+  {
+    "revision": "2131b4a81e6035c51491",
+    "url": "/static/js/runtime-main.3715cc6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
